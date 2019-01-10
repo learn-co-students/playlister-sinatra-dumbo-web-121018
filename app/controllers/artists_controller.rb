becc1063d7faa6ea12c1217b84b5bf3f40e99583
rhuwellsamano <rhuwell.samano@gmail.com>
2019-01-10 14:35:35 -0500

    new and index for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,20 @@
 class ArtistsController < ApplicationController
-  
+
+# Index Action
+    get '/artists' do
+      @artists = Artist.all
+      erb :'/artists/index'
+    end
+
+# New Action
+    get '/artists/new' do
+      erb :'/artists/new'
+    end
+
+# Create Action
+    post '/artists' do
+      @artist = Artist.create(params)
+      redirect "/artists/#{}"
+    end
+
 end
