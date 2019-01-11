22aab3a844ae8c379f80b28e32152dd3d536ef04
lsamano <leizl.samano@gmail.com>
2019-01-11 08:34:22 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,8 +6,26 @@ class SongsController < ApplicationController
   end
 
   # New
+  get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
+    # binding.pry
+    erb :'songs/new'
+  end
 
   # Create
+  post '/songs' do
+    # binding.pry
+    @song = Song.create(params["song"])
+    @song.update(artist_id: params["artist"]["id"])
+
+    #Need to add genres
+    params["genre"].each do |genre_id, genre_name|
+      SongGenre.create(song_id: @song.id, genre_id: genre_id)
+    end
+
+    # binding.pry
+  end
 
   # Show
   get '/songs/:slug' do
